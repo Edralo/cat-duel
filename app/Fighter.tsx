8bcacb6bd336e66c59eac4474c31513f9bee1499
Edralo <anthony.baussard@gmail.com>
2024-03-25 02:38:19 +0100

    (Add) Changes cursor to pointer when hovering cat to vote for

--- app/Fighter.tsx
+++ app/Fighter.tsx
@@ -15,7 +15,7 @@ export default function Fighter({cat, handleVote}: FighterProps) {
             width={500}
             height={500}
             alt="Picture of a cute cat"
-            className="size-60 md:size-80 lg:size-[30rem] rounded-full object-fill border-black border-8"
+            className="size-60 md:size-80 lg:size-[30rem] rounded-full object-fill border-black border-8 hover:cursor-pointer"
             onClick={() => handleVote(cat)}
         />
     )
